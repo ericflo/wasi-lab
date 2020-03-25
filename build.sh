8a87bf6d559cbb15966237458f9e0ef46e1f3e3c
Eric Florenzano <floguy@gmail.com>
2020-03-25 23:50:18 +0000

    Build in a temp dir

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Install wasienv
+set -e
+
+BUILD_DIR="$(pwd)/build"
+TMP_DIR="$(mktemp -d)"
+
+mkdir -p $BUILD_DIR
+
+echo "Building in $TMP_DIR"
+cp -R /etc/wasi-lab $TMP_DIR
+cd $TMP_DIR/wasi-lab
 #curl https://raw.githubusercontent.com/wasienv/wasienv/master/install.sh | sh
-
 mkdir -p build
 cd build
 wasimake cmake ..
 cd ..
 make -C build
+cp ./build/duk.wasm $BUILD_DIR
+rm -Rf "$TMP_DIR"
